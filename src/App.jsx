77c80ea892c3ff63154f4a185f67c99557ae0a22
Winterks <winterksw@gmail.com>
2025-04-04 15:27:40 +0100

    attempt to speak intro on startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,48 @@
-import GenericPlayer from "./components/birds/GenericPlayer";
 import { useEffect, useRef, useState } from "react";
-
+import GenericPlayer from "./components/birds/GenericPlayer";
 import "./App.css";
 import parseCSV from "./components/birds/parseCSV";
 
-// var birdHasBeenClicked = false;
+var introText = 'The application will scroll through images of each bird in turn.' + 
+                  ' Click on the bird you want to hear and its song will be played.' +
+                  " If the bird is clicked a second time while playing, it will stop" +
+                  " the audio and scroll to the next bird.";
 var birdies = [];
-parseCSV("data.csv",birdies); 
-
-// initialise_data();
+parseCSV("data.csv", birdies);
 
 function App() {
-  const indexRef = useRef(0); // Track the current index
+  const indexRef = useRef(0);
   const birdHasBeenClicked = useRef(false);
+  const introRequired = useRef(true);
   const [currentBird, setCurrentBird] = useState(null);
-  const [startSpeaking, setStartSpeaking,audioEnded] = useState(false);
-  const [triggerRecursion, setTriggerRecursion] = useState(false);
+  const [startSpeaking, setStartSpeaking] = useState(false);
+  const [triggerRecursion, setTriggerRecursion] = useState(false); 
 
   useEffect(() => {
     if (!birdies.length) {
       return;
     }
 
-    const speakNext = () => {
-      if (indexRef.current >= birdies.length && !birdHasBeenClicked.current) {
-        setCurrentBird(null);
-        setStartSpeaking(!startSpeaking);
-        return; // Stop when all birds are spoken
-      }
-
-      if (birdHasBeenClicked.current) {
-        return;
-      }
-
-      const bird = birdies[indexRef.current];
-
-      setCurrentBird(bird); // Set the current bird
-
-      const speech = new SpeechSynthesisUtterance(bird.preview);
-
-      speech.onend = () => {
-        indexRef.current++; // Move to the next bird
-        setTimeout(() => {
-          speakNext();
-        }, 2500); // Delay before speaking the next one
-      };
-
-      window.speechSynthesis.speak(speech);
-    }; // end of speakNext
-
-    if (startSpeaking) speakNext(); // Start speaking
+    if (startSpeaking) speakNext();
   }, [birdies, startSpeaking, triggerRecursion]);
 
-  return (
+  return (  
     <div className="bird-list">
       {startSpeaking ? null : (
-        <button
-          className="start-button"
+        <button className="start-button" 
           onClick={() => {
             setStartSpeaking(!startSpeaking);
-            indexRef.current = 0; // Reset the index
-          }}
-        >
-          <span>British bird songs<br />
-            Click to start</span>
+            indexRef.current = 0; // reset the index
+          }}>
+        <span>
+          British bird songs<br />
+          {introText}
+          <br /> Click to start
+          {introRequired.current ? speakStart() : null }
+        </span>
         </button>
       )}
-      {startSpeaking &&
-        birdies.map((bird, index) => (
+      {startSpeaking && birdies.map((bird, index) => (
           <div
             key={index}
             onClick={() => {
@@ -90,6 +65,45 @@ function App() {
         ))}
     </div>
   );
+
+  function speakStart() {
+    const intro = new SpeechSynthesisUtterance(introText);
+    window.speechSynthesis.speak(intro);
+    
+    intro.onend = () => {
+      console.log("start message");
+    };
+  }
+
+  function speakNext() {
+    if (indexRef.current >= birdies.length && !birdHasBeenClicked.current) {
+      setCurrentBird(null);
+      setStartSpeaking(!startSpeaking);
+      introRequired.current = false;
+      introText = null;
+      return;
+    }
+
+    if (birdHasBeenClicked.current) {
+      return;
+    }
+
+    const bird = birdies[indexRef.current];
+
+    setCurrentBird(bird);
+
+    const speech = new SpeechSynthesisUtterance(bird.preview);
+
+    speech.onend = () => {
+      indexRef.current++;
+      setTimeout(() => {
+        speakNext();
+      }, 2500);
+    };
+
+    window.speechSynthesis.speak(speech);
+  } // end of speaknext
+
 }
 
 export default App;
--- src/components/birds/GenericPlayer.jsx
+++ src/components/birds/GenericPlayer.jsx
@@ -4,16 +4,8 @@ import PropTypes from "prop-types";
 
 function GenericPlayer({ audioUrl, imageSrc, defaultState = false }) {
   const audioRef = useRef(new Audio(audioUrl));
-  // var audioRef = audioUrl;
   const [isPlaying, setIsPlaying] = useState(defaultState);
- /*  var base_url = window.location.origin + window.location.pathname;
-  var imageUrl = "";
 
-  if(imageSrc.includes("://")) { // assume full Url as protocol specified
-    imageUrl = new URL(imageSrc);
-  } else {
-    imageUrl = new URL(imageSrc,base_url); // base url required
-  }; */
   const handleClick = () => {
   
     if (isPlaying) {
@@ -21,21 +13,18 @@ function GenericPlayer({ audioUrl, imageSrc, defaultState = false }) {
       audioRef.current.currentTime = 0; // Reset audio to start
     } else {
       audioRef.current.play();
-      audioRef.current.onended = () => {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0; // Reset audio to start
-        debugger
-      }
     }
     setIsPlaying(!isPlaying);
   };
+  
+  audioRef.current.addEventListener("ended", () => {
+    setIsPlaying(true);
+    // simulate bird clicked so it continues onto next bird
+    // document.getElementByid("bird-button").click();
+    clickBird();
 
- /*  audioRef.current.addEventListener('ended', function() {
-    console.log('Audio finished playing '+ audioUrl);
-    // Perform actions when the audio ends
-    setIsPlaying(!isPlaying);
-    debugger
-  }); */
+  });
+
   audioRef.current.addEventListener('error', function(error) {
     console.error('An error occurred:', error);
   });
